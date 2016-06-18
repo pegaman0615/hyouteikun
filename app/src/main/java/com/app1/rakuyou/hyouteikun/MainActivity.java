1da0f08b8c9a4b85032163c2aa94881b152a534d
temma <pegaman@icloud.com>
2016-06-18 11:04:43 +0900

    build main activity ver1

--- app/src/main/java/com/app1/rakuyou/hyouteikun/MainActivity.java
+++ app/src/main/java/com/app1/rakuyou/hyouteikun/MainActivity.java
@@ -2,6 +2,7 @@ package com.app1.rakuyou.hyouteikun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //タイトルバー非表示
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // レイアウトセット
+        setContentView(R.layout.activity_main);
+
     }
 }
