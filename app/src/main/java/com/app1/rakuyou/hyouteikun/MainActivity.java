ed46d39d10df9b95f623a5ed67f128d76fa7a7d4
temma <pegaman@icloud.com>
2016-06-18 12:30:39 +0900

    add 4 activity and create move activity code

--- app/src/main/java/com/app1/rakuyou/hyouteikun/MainActivity.java
+++ app/src/main/java/com/app1/rakuyou/hyouteikun/MainActivity.java
@@ -1,19 +1,44 @@
 package com.app1.rakuyou.hyouteikun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         //タイトルバー非表示
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        // レイアウトセット
+        // レイアウトをセットする
         setContentView(R.layout.activity_main);
+    }
+    public void onClick(View V) {
+        switch (V.getId()) {
+
+            case R.id.button://"始める"ボタンを押した場合
+                //Main2Activityに移動
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                //↓移動開始
+                startActivity(intent);
+                break;
+            case R.id.button2://"使い方"ボタンを押した場合
+                //Main3Activityに移動
+                Intent intent1 = new Intent(MainActivity.this, Main3Activity.class);
+                //↓移動開始
+                startActivity(intent1);
+                break;
+            case R.id.button3://"設定"ボタンを押した場合
+                //Main4Activityに移動
+                Intent intent2 = new Intent(MainActivity.this, Main4Activity.class);
+                //↓移動開始
+                startActivity(intent2);
+                break;
+        }
     }
 }
